ec03451ec45dc97786aeb879cca9f61302f30138
13910 <13910@mitoma.dhcp.wustl.edu>
2024-08-31 14:36:55 -0500

    new file

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -3,13 +3,13 @@ package studio0;
 public class BurritoBudget {
 
 		public static void main(String args[]) {
-		int burritosEatenWeek1 = 7;
-		int burritosEatenWeek2 = 4;
-		int burritosEatenWeek3 = 19; //don't judge me
-		int burritosEatenWeek4 = 1;
+		int burritosEatenWeek1 = 171;
+		int burritosEatenWeek2 = 24;
+		int burritosEatenWeek3 = 129; //don't judge me
+		int burritosEatenWeek4 = 13;
 		
 		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
-		double averageWeeklyBurritos = totalBurritos / 4.00;
+		double averageWeeklyBurritos = totalBurritos / 4.0000;
 		
 		double burritoPrice = 7.50;
 		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
